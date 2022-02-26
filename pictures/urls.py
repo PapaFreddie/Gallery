5e8dfa4db4b4a38c76989c732e8f4a8c17723297
PapaFreddie <fred.papa@student.moringaschool.com>
2022-02-26 15:14:36 +0300

    linked viewpicture and gallery urls

--- pictures/urls.py
+++ pictures/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery, name='gallery'),
-    path('picture/<int:pk>', views.viewPicture, name='picture'),
+    path('picture/<str:pk>/', views.viewPicture, name='picture'),
     path('add/', views.addPicture, name='add')
 ]
   
