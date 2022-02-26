cb0d9585b0eccc5dcc34ffc8f83a7196c5cc8ac4
PapaFreddie <fred.papa@student.moringaschool.com>
2022-02-26 15:41:32 +0300

    created superuser to access app admin

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,22 @@
+from calendar import c
+from email.mime import image
+from operator import truediv
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Picture(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
+
+ 
