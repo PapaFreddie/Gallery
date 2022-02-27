22ca70b1734a23202e8ace6cd2437ae1a1accc7a
PapaFreddie <fred.papa@student.moringaschool.com>
2022-02-27 13:29:03 +0300

    added functionality to add template

--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,8 @@
+from email.mime import image
 from multiprocessing import context
 from signal import pthread_kill
-from django.shortcuts import render
+from unicodedata import category
+from django.shortcuts import render, redirect
 from .models import Category, Picture
 
 # Create your views here.
@@ -18,5 +20,34 @@ def viewPicture(request, pk):
 
 
 def addPicture(request):
-    return render(request, 'pictures/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        
+        print('data:', data)
+        print('image:', image)
+
+        if data['category'] != 'none': 
+            category = Category.objects.get(id = data['category'])
+        elif data ['category_new'] != '':
+            category, created = Category.objects.get_or_create(name = data['category_new'])
+        else:
+            category = None
+        
+        picture = Picture.objects.create(
+            category = category,
+            description = data['description'],
+            image = image,
+
+            )
+
+        return redirect('gallery')
+
+
+
+    context = {'categories': categories}
+
+    return render(request, 'pictures/add.html', context)
 
